277a81fe0cd4867d7ecbe1b7b04432a17951f649
Dzianis Bunchanka <Dzianis_Bunchanka@epam.com>
2020-01-17 21:03:28 +0300

    add validation form with localStorage

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import { createForm } from "./createForm.js";
+import { addFormSubmitEvent } from "./addFormEvents.js";
+
+if (!localStorage.getItem("isValidationRequired")) {
+  createForm();
+  addFormSubmitEvent();
+} else {
+  console.log("СИСТЕМА ВАС ЗАПОМНИЛА, ВХОД УСПЕШЕН");
+}
