4dc54ec8f0b7be02f728eb7be59e1f76113c6eb8
Dzianis Bunchanka <Dzianis_Bunchanka@epam.com>
2020-01-21 21:16:10 +0300

    add ability to show detailed hero information

--- src/js/DOMhandlers.js
+++ src/js/DOMhandlers.js
@@ -13,11 +13,12 @@ export const renderHeroes = superheroesArray => {
   });
 };
 
-export const insertHeroToContainer = ({ name, images: { md } }, index) => {
+export const insertHeroToContainer = ({ id, name, images: { md } }, index) => {
   // шаг 1 - создаю heroContainer
   const heroContainer = document.createElement("div");
   heroContainer.className = "superhero_container";
   heroContainer.id = index;
+  heroContainer.dataset.heroId = id;
   // шаг 2 - создаю картинку
   const heroImg = document.createElement("img");
   heroImg.src = md;
@@ -29,6 +30,9 @@ export const insertHeroToContainer = ({ name, images: { md } }, index) => {
   heroContainer.appendChild(heroTitle);
   // шаг 5 - втсавляю heroCotainer в heroesContainer
   document.querySelector(".superheroes_container").appendChild(heroContainer);
+  heroContainer.addEventListener("click", e => {
+    fetchHeroData(e.currentTarget.dataset.heroId);
+  });
 };
 
 export const createSlideButtons = () => {
@@ -44,3 +48,54 @@ export const createSlideButtons = () => {
   buttonsContainer.appendChild(rightButton);
   ROOT_ELEMENT.appendChild(buttonsContainer);
 };
+
+const fetchHeroData = heroId => {
+  return fetch(
+    `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${heroId}.json`
+  )
+    .then(data => data.json())
+    .then(
+      ({
+        name,
+        powerstats,
+        appearance: { race },
+        biography: { fullName },
+        images: { lg: heroImage }
+      }) => createHero(name, powerstats, race, fullName, heroImage)
+    )
+    .then(() => {
+      createGoBackButton();
+    });
+};
+
+const createHero = (name, powerstats, race, fullName, heroImage) => {
+  removeSlider();
+  const heroContainer = document.createElement("div");
+  heroContainer.className = "heroContainer";
+  heroContainer.innerHTML = `
+    <h1>${name}</h1>
+    <div class="title">${fullName}</div>
+    <img src=${heroImage} />
+  `;
+  document.getElementById("root").appendChild(heroContainer);
+};
+
+const removeSlider = () => {
+  document.querySelector(".superheroes_container").style.display = "none";
+  document.querySelector(".buttons_container").style.display = "none";
+};
+
+const showSlider = () => {
+  document.querySelector(".superheroes_container").style.display = "flex";
+  document.querySelector(".buttons_container").style.display = "flex";
+  document.querySelector('.heroContainer').remove();
+  document.querySelector('.goBackBtn').remove();
+}
+
+const createGoBackButton = () => {
+  const button = document.createElement('button');
+  button.className = 'goBackBtn';
+  button.innerHTML = 'Go Back';
+  button.addEventListener('click', showSlider)
+  document.getElementById("root").appendChild(button);
+}
